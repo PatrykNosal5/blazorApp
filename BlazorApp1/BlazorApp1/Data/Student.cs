a80326bf82455215fa000c9290dd249606c09ecd
Patryk Nosal <s25166@pjwstk.edu.pl>
2023-06-07 23:59:06 +0200

    final

---
+++ BlazorApp1/BlazorApp1/Data/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp1.Data
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; } = null!; 
+        public string LastName { get; set; } = null!;
+        public string Avatar { get; set; } = null!;
+        public DateTime BirthDate { get; set; }
+        public string Studies { get; set; }
+    }
+}
---
+++ BlazorApp1/BlazorApp1/Data/StudentsService.cs
@@ -0,0 +1,46 @@
+﻿namespace BlazorApp1.Data
+{
+    public class StudentsService
+    {
+        public List<Student> Students { get; }
+        public StudentsService() { 
+
+            Students = new List<Student>()
+        {
+        new Student() {
+                ID = 1,
+                Avatar ="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png",
+                FirstName = "Jan",
+                LastName="Kowalski",
+                BirthDate = DateTime.Now,
+                Studies = "Informatyka"
+        },
+        new Student()
+        {
+            ID = 2,
+                Avatar ="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png",
+                FirstName = "Halina",
+                LastName="Jankowska",
+                BirthDate = DateTime.Now,
+                Studies = "SNM"
+        },  
+        new Student() {
+                ID = 3,
+                Avatar ="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png",
+                FirstName = "Janina",
+                LastName="Kowalska",
+                BirthDate = DateTime.Now,
+                Studies = "Informatyka"
+        },
+        new Student() {
+                ID = 4,
+                Avatar ="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png",
+                FirstName = "Jakub",
+                LastName="Nowak",
+                BirthDate = DateTime.Now,
+                Studies = "SNM"
+        },
+        };
+        }
+    }
+}
